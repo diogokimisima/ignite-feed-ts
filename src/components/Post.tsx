cf80784dde5fef1efcbc9d28a34471aa33f243d4
dkimisima@gmail.com <dkimisima@gmail.com>
2024-09-24 11:00:10 -0300

    modifying from jsx to tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,9 +4,27 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
-import { useState } from 'react'
+import { FormEvent, useState, ChangeEvent, InvalidEvent } from 'react'
 
-export function Post({ author, content, publishedAt }) {
+interface Author {
+    name: string
+    avatar: string
+    avatarUrl: string
+    role: string
+}
+
+interface Content {
+    type: 'paragraph' | 'link'
+    content: string
+}
+
+interface PostProps {
+    author: Author
+    publishedAt: Date
+    content: Content[]
+}
+
+export function Post({ author, content, publishedAt }: PostProps) {
     const [comments, setComments] = useState([
         'Post muito bacana!'
     ])
@@ -22,19 +40,19 @@ export function Post({ author, content, publishedAt }) {
         addSuffix: true
     })
 
-    function handleCreateNewComment(event) {
+    function handleCreateNewComment(event: FormEvent) {
         event.preventDefault()
 
         setComments([...comments, newCommentText])
         setNewCommentText('')
     }
 
-    function handleNewCommentChange() {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
     }
 
-    function deleteComment(commentToDelete) {
+    function deleteComment(commentToDelete: string) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
             return comment !== commentToDelete
         })
@@ -42,7 +60,7 @@ export function Post({ author, content, publishedAt }) {
         setComments(commentsWithoutDeletedOne)
     }
 
-    function handleNewCommentInvalid() {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
